01c1797427a4f3b34e73cb4eaf4e357fee0ed006
Musica-R <musicaramesh31102002@gmail.com>
2025-12-08 12:41:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ import cors from "cors";
 const app = express ();
 
 app.use(cors({
-   origin: "https://form-registration-login-123.netlify.app/"
-}))
+  origin: "https://form-registration-login-123.netlify.app",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true // if you send cookies
+}));
 app.use(express.json());
 app.use("/api/user",router);
 
