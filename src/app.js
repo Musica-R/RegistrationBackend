98980f00095c7ed9cfa934b1dab8eba50991c0dd
Musica-R <musicaramesh31102002@gmail.com>
2025-12-08 14:15:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,36 +4,9 @@ import cors from "cors";
 
 const app = express ();
 
-app.use(
-  cors({
-    origin: "https://form-registration-login-123.netlify.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://form-registration-login-123.netlify.app"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+app.use(cors({
+   origin: "https://form-registration-login-123.netlify.app"
+}))
 app.use(express.json());
 app.use("/api/user",router);
 
