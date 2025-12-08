95db96fe5e0c115e87035c93318e6f88df04e625
Musica-R <musicaramesh31102002@gmail.com>
2025-12-08 14:09:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,28 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://form-registration-login-123.netlify.app"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use("/api/user",router);
